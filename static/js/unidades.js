d89aa4e8c8e5bb3940037b8fd60773f0a27c419c
CodeBase <code@base.net>
2023-12-02 19:34:29 +0100

    .

---
+++ static/js/unidades.js
@@ -0,0 +1,106 @@
+/*************************************************************************************************************************************************************************************************************
+ * DEFINICION DE LAS UNIDADES DEL TEMARIO
+ * Array de unidades con un campo "nombre" que contiene otro array con las preguntas, posibles Respuestas
+ * y el num. de respuesta correcta.
+ *************************************************************************************************************************************************************************************************************/
+export const unidades = [
+    // UNIDAD 1
+    {
+        //idUnidad: 1,
+        titulo: "Los derechos del trabajo",
+        preguntas: [
+            {
+                enunciado: "Señala cuáles de los siguientes derechos están considerados derechos fundamentales por la Constitución española de 1978:",
+                posiblesRespuestas: [
+                    "Derecho al trabajo y a un salario",
+                    "Derecho a la huelga y a la libertad sindical",
+                    "Derecho a la ocupación efectiva",
+                ],
+                numRespuestaCorrecta: 2,
+            },
+            {
+                enunciado: "Indica cuál de las siguientes situaciones está excluida del derecho laboral",
+                posiblesRespuestas: [
+                    "Una joven que trabaja en una compañia de seguros con un contrato mercantil y cobra comisión por cada póliza que realiza",
+                    "Un empleado al servicio del hogar familiar.",
+                    "Un auxiliar de farmacia",
+                    "Una deportista profesional",
+                ],
+                numRespuestaCorrecta: 1,
+            },
+            {
+                enunciado: "Enunciado 3",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                ],
+                numRespuestaCorrecta: 1,
+            },
+            {
+                enunciado: "Enunciado 4",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                ],
+                numRespuestaCorrecta: 1,
+            },
+            {
+                enunciado: "Enunciado 5",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                    "Respuesta 3",
+                    "Respuesta 4",
+                ],
+                numRespuestaCorrecta: 4,
+            },
+        ],
+    },
+    // UNIDAD 2
+    {
+        titulo: "Nombre Unidad 2",
+        preguntas: [
+            {
+                enunciado: "Enunciado 1",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                    "Respuesta 3",
+                ],
+                numRespuestaCorrecta: 1,
+            },
+            {
+                enunciado: "Enunciado 2",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                    "Respuesta 3",
+                ],
+                numRespuestaCorrecta: 2,
+            },
+        ],
+    },
+    {
+        titulo: "Nombre Unidad 3",
+        preguntas: [
+            {
+                enunciado: "Enunciado 1",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                    "Respuesta 3",
+                ],
+                numRespuestaCorrecta: 2,
+            },
+            {
+                enunciado: "Enunciado 2",
+                posiblesRespuestas: [
+                    "Respuesta 1",
+                    "Respuesta 2",
+                    "Respuesta 3",
+                ],
+                numRespuestaCorrecta: 1,
+            },
+        ],
+    }
+];
